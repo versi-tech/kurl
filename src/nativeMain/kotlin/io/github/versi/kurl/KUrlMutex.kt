706b7093f5602f6128afe74fda0c0ccdf7415c9d
versi-tech <kstanik@gmail.com>
2022-04-08 15:02:55 +0200

    Fixed missing mutex init calls and wrong mutex indexing.

--- src/nativeMain/kotlin/io/github/versi/kurl/KUrlMutex.kt
+++ src/nativeMain/kotlin/io/github/versi/kurl/KUrlMutex.kt
@@ -4,19 +4,21 @@ import kotlinx.cinterop.MemScope
 import kotlinx.cinterop.allocArray
 import kotlinx.cinterop.get
 import kotlinx.cinterop.ptr
-import platform.posix.pthread_mutex_lock
-import platform.posix.pthread_mutex_t
-import platform.posix.pthread_mutex_unlock
+import platform.posix.*
 
 class KUrlMutex(memScope: MemScope = MemScope(), mutexesCount: Long) {
 
-    private val mutexes = memScope.allocArray<pthread_mutex_t>(mutexesCount)
+    private val mutexes = memScope.allocArray<pthread_mutex_t>(mutexesCount).also {
+        for (i in 0 until mutexesCount) {
+            pthread_mutex_init(it[i].ptr, null)
+        }
+    }
 
     fun lock(data: Int) {
-        pthread_mutex_lock(mutexes[data].ptr)
+        pthread_mutex_lock(mutexes[data - 1].ptr)
     }
 
     fun unlock(data: Int) {
-        pthread_mutex_unlock(mutexes[data].ptr)
+        pthread_mutex_unlock(mutexes[data - 1].ptr)
     }
 }
